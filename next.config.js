d7e71b4d16ba3b950a6c60a90f62baf845c5aae1
GhostEsso <lilipitaham@gmail.com>
2025-01-03 12:18:12 +0000

    Configure for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/phil_dev',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
