955e5a2ac4408c4f3943153bc4e0a78486540b59
GhostEsso <lilipitaham@gmail.com>
2025-01-03 12:49:01 +0000

    i

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/phil_dev',
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/phil_dev/',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig 
